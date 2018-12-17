7c104cc4c14241e01b6393d2195a7c0f0a37abb1
Andrey Kim <drewkenjo@users.noreply.github.com>
2018-12-16 23:13:33 -0500

    unified format for consumers/producers

--- src/app.js
+++ src/app.js
@@ -5,15 +5,13 @@ import consumers from './consumers'
 let macd = new consumers.MACD()
 
 let binance = new producers.Binance(['ETHBTC'])
+
 binance.ready.then( () => {
   binance.add_consumer(macd)
 
   setInterval( () => {
     wsclients.forEach( cli => {
-      let inp = {}
-      let maIndices = [5,10,12,20,26]
-      maIndices.forEach(maIndex => inp['sma'+maIndex] = macd['sma'+maIndex])
-      cli.send(JSON.stringify(inp))
+      cli.send(JSON.stringify(macd.output()))
     })
   }, 3000)
 })
